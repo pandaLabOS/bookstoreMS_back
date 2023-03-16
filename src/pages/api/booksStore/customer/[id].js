35d00727269d30c4a72ef2688649c1052b41bd6a
JackNicolus <118882209+JackNicolus@users.noreply.github.com>
2023-03-16 16:51:09 +0700

    TestInit

--- src/pages/api/booksStore/customer/[id].js
+++ src/pages/api/booksStore/customer/[id].js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     }
 }
     const customerSchema = new Schema({
-        _id: String,
+        _id: Number,
         firstName: String,
         lastName: String,
         phoneNumber: String
