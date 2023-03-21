1315802df2dc1f38c7232cd5fdb13a4aa939aa92
pandaLabOS <u6215112@au.edu>
2023-03-21 17:37:44 +0700

    Changed connectionString at /books/index.js

--- src/pages/api/books/index.js
+++ src/pages/api/books/index.js
@@ -1,5 +1,5 @@
 import {connect, model, models, Schema} from "mongoose"
-const connectionString = "mongodb+srv://admin:bookstoreMS@bookstorems.qgl1qca.mongodb.net/bookstore"
+const connectionString = "mongodb+srv://user1:bookstoreUser1@bookstorems.qgl1qca.mongodb.net/bookstore"
 
 export default async function handler(req, res) {
     await connect(connectionString);
