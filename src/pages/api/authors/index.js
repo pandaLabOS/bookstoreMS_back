6a3d1d69304db9a19820ae35823ca1c38b2a6a40
JackNicolus <118882209+JackNicolus@users.noreply.github.com>
2023-03-21 13:09:35 +0700

    try catch

--- src/pages/api/authors/index.js
+++ src/pages/api/authors/index.js
@@ -6,20 +6,32 @@ export default async function handler(req, res) {
     console.log("req.method", req.method)
 
     if (req.method === 'GET') {
+        try{
         const docs = await Author.find()
         res.status(200).json(docs)
+        } catch (error){
+            console.error(error)
+        }
     } 
     
     else if (req.method === 'POST') {
+        try{
         const doc = await Author.create(req.body)
         res.status(201).json(doc)
-    } 
-    
+        } catch (error){
+            console.error(error)
+        } 
+    }
     else {
+        try{
         res.setHeader('Allow', ['GET', 'POST'])
         res.status(405).end(`Method ${req.method} Not Allowed`)
+        } catch (error){
+            console.error(error)
+        }
     }
 }
+
     const authorSchema = new Schema({
         id: String,
         firstName: String,
